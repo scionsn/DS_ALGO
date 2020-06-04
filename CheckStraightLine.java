147dc8902a3bfc3db2df5e3869a35fc45f4ea32d
scionsn <52966844+scionsn@users.noreply.github.com>
2020-06-05 00:40:15 +0530

    Update CheckStraightLine.java

--- CheckStraightLine.java
+++ CheckStraightLine.java
@@ -6,7 +6,7 @@
 package leetcode;
 
 /**
- *
+ *just for git purposes
  * @author SCION
  * You are given an array coordinates, coordinates[i] = [x, y], where [x, y] represents the coordinate of a point. Check if these points make a straight line in the XY plane.
  * e.g- Input: coordinates = [[1,2],[2,3],[3,4],[4,5],[5,6],[6,7]]
