f468cf70bc22e9f02842d3f5157442b4f4cd97f3
scionsn <52966844+scionsn@users.noreply.github.com>
2020-06-23 01:04:36 +0530

    done

---
+++ ClimbStairs.java
@@ -0,0 +1,20 @@
+
+public class ClimbStairs {
+	 public int climbStairs(int n) {
+	        int arr[]=new int[n+1];
+	        
+	        arr[0]=1;
+	        
+	        arr[1]=2;
+	        for(int i=2;i<n;i++){
+	            arr[i]=arr[i-1]+arr[i-2];
+	            
+	        }
+	        return arr[n-1];
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
