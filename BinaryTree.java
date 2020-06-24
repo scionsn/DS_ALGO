63273aa4a229f07745a581dc339197302cd18385
scionsn <52966844+scionsn@users.noreply.github.com>
2020-06-24 16:34:26 +0530

    new lc commit

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,6 +1,8 @@
 
-package ds_algo;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 /**
  *
@@ -32,6 +34,51 @@ str="left";
         node.right=insert();
         return node;
         
+    }
+    BinaryTree search(BinaryTree root,int key){
+       if (root == null)  
+        return null;  
+  
+    if (root.data == key)  
+        return root;  
+    BinaryTree left=search(root.left,key);
+    if(left!=null){
+        return root.left;
+    }
+    else{
+        BinaryTree right=search(root.right,key);
+        return right;
+    }
+    
+  
+        
+   
+  
+//    // then recur on left sutree / 
+//    boolean res1 = search(root.left, key);  
+//    if(res1) return true; // node found, no need to look further 
+//  
+//    // node is not found in left, so recur on right subtree / 
+//    boolean res2 = search(root.right, key);  
+//  
+//    return res2;  
+           
+        
+        
+       
+    }
+    BinaryTree mirrorimg(BinaryTree root){
+        if(root==null){
+            return null;
+        }
+        else{
+            mirrorimg(root.left);
+            mirrorimg(root.right);
+            BinaryTree temp=root.left;
+            root.left=root.right;
+            root.right=temp;
+        }
+        return root;
     }
     void print(BinaryTree root){
         if(root==null){
@@ -102,6 +149,72 @@ str="left";
           }
                         return max+1;
       }
+       void verticalview(BinaryTree root){
+            
+if(root==null){
+    return; 
+} 
+int height=0;
+TreeMap<Integer,ArrayList<Integer>> tree=new TreeMap<Integer, ArrayList<Integer>>();
+getverticalview(root,tree,height);
+ for (Map.Entry<Integer, ArrayList<Integer>> cm : tree.entrySet()) {
+      System.out.println(cm.getValue());
+    }
+  
+        }
+        void getverticalview(BinaryTree root,TreeMap<Integer,ArrayList<Integer>> tree,int height){
+            if(root==null){
+                return;
+            }
+            if(tree.get(height)==null){
+                      ArrayList<Integer> list = new ArrayList<Integer>();
+                      list.add(root.data);
+                      tree.put(height,list);
+
+            }
+            else
+            {
+                ArrayList<Integer> l=tree.get(height);
+                l.add(root.data);
+                tree.put(height,l);
+      tree.put(height, l);
+            }
+            getverticalview(root.left,tree, height-1);
+            getverticalview(root.right,tree, height+1);
+            
+        }
+        public int countNodes(BinaryTree root) {
+        	int count=0;
+            if(root==null)
+            	return 0;
+            else {
+            	count++;
+
+            	return 1+countNodes(root.left)+countNodes(root.right);
+
+            }
+            
+           
+        }
+       BinaryTree  LCA(BinaryTree root,int p,int q){
+            if(root==null){
+                return null;
+            }
+            if(root.data==p||root.data==q){
+                return root;
+            }
+            BinaryTree left=LCA(root.left,p,q);
+            BinaryTree right=LCA(root.right,p,q);
+            if(left!=null&&right!=null){
+                return root;
+            }  
+            if(left==null){
+                return right;
+            }
+            else 
+                return left;
+     
+        }
 //    
     public static void main(String[] args) {
         BinTreeOp node=new BinTreeOp();
@@ -109,14 +222,21 @@ str="left";
                 System.out.println("tree print==>");
 
         node.print(root);
-        System.out.println("preorder traversal==>");
-        node.preorder(root);
-                System.out.println("postorder traversal==>");
-                node.postorder(root);
-                        System.out.println("inorder traversal==>");
-node.inorder(root);
-int height=node.getheight(root);
-        System.out.println("the height of binary tree is "+height);
+System.out.println(node.search(root, 3)!=null?"exists":"not exists");
+node.LCA(root, 3, 4);
+BinaryTree ans=node.LCA(root, 3, 4);
+        System.out.println("lca is "+ans.data);
+        System.out.println(node.countNodes(root));
+//        node.verticalview(root);
+//        System.out.println("preorder traversal==>");
+//        node.preorder(root);
+//                System.out.println("postorder traversal==>");
+//                node.postorder(root);
+        
+//                        System.out.println("inorder traversal==>");
+//node.inorder(root);
+//int height=node.getheight(root);
+//        System.out.println("the height of binary tree is "+height);
 
         
                 
