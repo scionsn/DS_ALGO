2d11d208e09fbf6a5e9a062f6772545d381f4bf8
scionsn <52966844+scionsn@users.noreply.github.com>
2020-12-24 17:18:57 +0530

    dec lc

---
+++ BalancedBinaryTree.java
@@ -0,0 +1,23 @@
+
+public class BalancedBinaryTree {
+	  public boolean isBalanced(TreeNode root) {
+	        if(root==null)
+	            return true;
+	return helper(root)!=-1;
+	    }
+	    int helper(TreeNode root){
+	        if(root==null)
+	            return 0;
+	        int left=helper(root.left);
+	        int right=helper(root.right);
+	        if(left==-1||right==-1||Math.abs(left-right)>1)
+	            return -1;
+	        return Math.max(left,right)+1;
+	        
+	    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
